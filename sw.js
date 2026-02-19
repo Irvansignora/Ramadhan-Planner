2680bcb60cddc84bd64f212a79e50c204bb42df2
Muhamad Irpan Yasin <143773773+Irvansignora@users.noreply.github.com>
2026-02-20 04:29:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ramadhan-v6';
+const CACHE = 'ramadhan-v7';
 const CORE  = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
